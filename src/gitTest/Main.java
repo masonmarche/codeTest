d1f28567cd0e682c285cf27de581aed832ed5d12
Mason Marche <MarcheMA19@pwcs-edu.org>
2019-01-02 13:53:30 -0500

    first

---
+++ src/gitTest/Main.java
@@ -0,0 +1,11 @@
+package gitTest;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main (String [] args) {
+		System.out.print("What is your name?");
+		System.out.print((new Scanner(System.in)).nextLine());
+	}
+}
