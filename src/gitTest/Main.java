7268c9888ad0b859ae1592cf2c8092a0ab0a59f3
Mason Marche <masonmarche@gmail.com>
2019-01-04 13:04:51 -0500

    added errors

--- src/gitTest/Main.java
+++ src/gitTest/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
 	public static void main (String [] args) {
 		System.out.print("What is your name?");
-		System.out.print((new Scanner(System.in)).nextLine());
+		System.out.print((new Scanner(System.in)).nextLine())
+
+				/ this is comment
 	}
 }
